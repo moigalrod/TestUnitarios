17de84171c961ad2050661f79e229327a6c26e83
Moisés Gallardo Rodríguez <moises.gallardorodriguez@plexus.local>
2022-01-18 13:44:04 +0100

    Codicionales en test con etiquetas

--- src/test/java/org/test/junitapp/ejemplo/models/CuentaTest.java
+++ src/test/java/org/test/junitapp/ejemplo/models/CuentaTest.java
@@ -1,18 +1,43 @@
 package org.test.junitapp.ejemplo.models;
 
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import jdk.jfr.Enabled;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.condition.*;
 import org.test.junitapp.ejemplo.exceptions.DineroInsuficienteException;
 
+import javax.xml.namespace.QName;
 import java.math.BigDecimal;
+import java.util.Properties;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CuentaTest {
 
+    Cuenta cuenta;
+
+    @BeforeEach
+    void InitMetodoTest() {
+        System.out.println("Inicialización de Tests");
+        this.cuenta = new Cuenta("Andres", new BigDecimal("1100.12345"));
+    }
+
+    @AfterEach
+    void FinMetodoTest() {
+        System.out.println("Finalizando el metodo");
+    }
+
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println("Init beforeAll");
+
+    }
+
+    @AfterAll
+    static void afterAll() {
+        System.out.println("Fin afterAll");
+    }
+
     @Test
-    @Disabled
     @DisplayName("Test Nombre Cuenta")
     void testNombreCuenta() {
         Cuenta cuenta = new Cuenta();
@@ -27,7 +52,7 @@ class CuentaTest {
     @Test
     @DisplayName("Test Saldo Cuenta")
     void testSaldoCuenta() {
-        Cuenta cuenta = new Cuenta("Andres", new BigDecimal("13.44444"));
+        Cuenta cuenta = new Cuenta("John Doe", new BigDecimal("13.44444"));
         assertEquals(13.44444, cuenta.getSaldo().doubleValue());
         assertFalse(cuenta.getSaldo().compareTo(BigDecimal.ZERO) < 0);
     }
@@ -43,28 +68,25 @@ class CuentaTest {
     @Test
     @DisplayName("Test Debito Cuenta")
     void testDebitoCuenta() {
-        Cuenta cuenta = new Cuenta("Andres", new BigDecimal("1000.12345"));
         cuenta.debito(new BigDecimal(100));
         assertNotNull(cuenta.getSaldo());
-        assertEquals(900, cuenta.getSaldo().intValue());
-        assertEquals("900.12345", cuenta.getSaldo().toPlainString());
+        assertEquals(1000, cuenta.getSaldo().intValue());
+        assertEquals("1000.12345", cuenta.getSaldo().toPlainString());
     }
 
     @Test
     @DisplayName("Test Credito Cuenta")
     void testCreditoCuenta() {
-        Cuenta cuenta = new Cuenta("Andres", new BigDecimal("1000.12345"));
         cuenta.credito(new BigDecimal(100));
         assertNotNull(cuenta.getSaldo());
-        assertEquals(1100, cuenta.getSaldo().intValue());
-        assertEquals("1100.12345", cuenta.getSaldo().toPlainString());
+        assertEquals(1200, cuenta.getSaldo().intValue());
+        assertEquals("1200.12345", cuenta.getSaldo().toPlainString());
     }
 
     @Test
     @DisplayName("Test Insuficiente ExceptionCuenta")
     void dineroInsuficienteExceptionCuenta() {
-        Cuenta cuenta = new Cuenta("Andres", new BigDecimal("1000.12345"));
-        Exception e = assertThrows(DineroInsuficienteException.class, () -> cuenta.debito(new BigDecimal("1001.12345")));
+        Exception e = assertThrows(DineroInsuficienteException.class, () -> cuenta.debito(new BigDecimal("1101.12345")));
         String actual = e.getMessage();
         String esperado = "Dinero inSuficiente";
         assertEquals(esperado, actual);
@@ -87,5 +109,42 @@ class CuentaTest {
                 , () -> assertEquals("Banco del estado", cuenta1.getBanco().getNombre())
                 , () -> assertTrue(b.getCuentas().stream().anyMatch(c -> c.getPersona().equals("Andres"))));
 
+    }
+
+    @Test
+    @EnabledOnOs(OS.WINDOWS)
+    void testSoloWindows() {
+    }
+
+    @Test
+    @EnabledOnJre(JRE.JAVA_8)
+    void testSoloJava8() {
+    }
+
+    @Test
+    @EnabledOnJre(JRE.JAVA_17)
+    void testSoloJava17() {
+    }
+
+    @Test
+    @EnabledOnOs({OS.LINUX, OS.MAC})
+    void testSoloLinuxMAc() {
+    }
+
+    @Test
+    @DisabledOnJre(JRE.JAVA_17)
+    void testNoJDK17() {
+    }
+
+    @Test
+    @Disabled
+    void imprimirSystemProperties() {
+        Properties p = System.getProperties();
+        p.forEach((k, v) -> System.out.println(k + " : " + v));
+    }
+
+    @Test
+    @EnabledIfSystemProperty(named = "java.version", matches = "17.0.1")
+    void testJavaVErsion17(){
     }
 }
